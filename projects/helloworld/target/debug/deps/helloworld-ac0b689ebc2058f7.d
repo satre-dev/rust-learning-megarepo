f97a476e54de3fad99f9d629fc6e25d6f0626cd3
satre-dev <sam.tredgett@gmail.com>
2025-06-04 18:07:59 +0100

    updated gitignore

--- projects/helloworld/target/debug/deps/helloworld-ac0b689ebc2058f7.d
+++
@@ -1,5 +0,0 @@
-/home/satre/Documents/code/rust-programming-megarepo/projects/helloworld/target/debug/deps/helloworld-ac0b689ebc2058f7: src/main.rs
-
-/home/satre/Documents/code/rust-programming-megarepo/projects/helloworld/target/debug/deps/helloworld-ac0b689ebc2058f7.d: src/main.rs
-
-src/main.rs:
