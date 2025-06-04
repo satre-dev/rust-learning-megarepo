6ad309ef3313a8fa2fe145c9ea67cbe154e45a26
satre-dev <sam.tredgett@gmail.com>
2025-06-04 18:04:14 +0100

    test

---
+++ projects/helloworld/target/debug/deps/helloworld-ac0b689ebc2058f7.d
@@ -0,0 +1,5 @@
+/home/satre/Documents/code/rust-programming-megarepo/projects/helloworld/target/debug/deps/helloworld-ac0b689ebc2058f7: src/main.rs
+
+/home/satre/Documents/code/rust-programming-megarepo/projects/helloworld/target/debug/deps/helloworld-ac0b689ebc2058f7.d: src/main.rs
+
+src/main.rs:
